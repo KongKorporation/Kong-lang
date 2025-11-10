73bca5384c9ae829afab387419c19d45b031bdef
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-11-10 15:35:28 +0100

    feat: Parsing/Include/Compilation errors are printed in red color

--- src/ErrorPrinter.hs
+++ src/ErrorPrinter.hs
@@ -8,29 +8,31 @@ import System.Exit (exitFailure)
 import Compiler.Include (IncludeError(..))
 import Compiler.Type.Error (ProgramError(..), prettyError)
 
+printInColor :: String -> String
+printInColor str = "\ESC[91m" ++ str ++ "\ESC[0m"
+
 -- Print an include error to stderr and exit
 printIncludeError :: IncludeError -> IO ()
 printIncludeError (FileNotFound path) =
-    hPutStrLn stderr ("[Include error] File not found: " ++ path) >> exitFailure
+    hPutStrLn stderr (printInColor("[Include error]") ++ " File not found: " ++ path) >> exitFailure
 printIncludeError (CircularDependency file chain) =
-    hPutStrLn stderr ("[Include error] Circular dependency detected in " ++ file ++
+    hPutStrLn stderr (printInColor("[Include error]") ++ " Circular dependency detected in " ++ file ++
                      ": " ++ show chain) >> exitFailure
 printIncludeError (ParseError _ msg) =
-    hPutStrLn stderr ("[Parsing error] " ++ msg) >> exitFailure
+    hPutStrLn stderr (printInColor("[Parsing error] ") ++ msg) >> exitFailure
 printIncludeError (MissingInclude file missing) =
-    hPutStrLn stderr ("[Include error] File '" ++ file ++ "' includes '" ++ missing ++
+    hPutStrLn stderr (printInColor("[Include error]") ++ " File '" ++ file ++ "' includes '" ++ missing ++
                      "' but it was not provided in compilation arguments") >> exitFailure
 printIncludeError (MissingSymbol requester included symbol) =
-    hPutStrLn stderr ("[Include error] File '" ++ requester ++ "' requests symbol '" ++ symbol ++
+    hPutStrLn stderr (printInColor("[Include error]") ++ " File '" ++ requester ++ "' requests symbol '" ++ symbol ++
                      "' from '" ++ included ++ "' but it does not exist") >> exitFailure
 printIncludeError (DuplicateSymbol symbol files) =
-    hPutStrLn stderr ("[Include error] Symbol '" ++ symbol ++ "' is defined in multiple files: " ++
+    hPutStrLn stderr (printInColor("[Include error]") ++ " Symbol '" ++ symbol ++ "' is defined in multiple files: " ++
                      show files) >> exitFailure
 
 -- Print compilation errors to stderr and exit
 printCompileError :: [ProgramError] -> IO ()
-printCompileError errs = do
-    hPutStrLn stderr "[Compilation error]"
+printCompileError errs =
     mapM_ (\(ProgramError file _ err) -> 
-        hPutStrLn stderr $ "  In file '" ++ file ++ "': " ++ prettyError err) errs
-    exitFailure
+        hPutStrLn stderr $ printInColor("[Compilation error]") ++ " In file '" ++ file ++ "': " ++ prettyError err) errs
+    >> exitFailure
